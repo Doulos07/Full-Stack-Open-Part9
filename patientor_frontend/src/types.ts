fdcd3dc80e0cdf6d76b4fa7531d1b7af52079c85
Santiago Ariel Alvarez <santiiariel2004@gmail.com>
2026-07-10 14:33:57 -0300

    Exercise 9.23

--- patientor_frontend/src/types.ts
+++ patientor_frontend/src/types.ts
@@ -7,8 +7,57 @@ export interface Diagnosis {
 export enum Gender {
   Male = "male",
   Female = "female",
-  Other = "other"
+  Other = "other",
 }
+export interface Diagnose {
+  code: string;
+  name: string;
+  latin?: string;
+}
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+
+  diagnosisCodes?: Array<Diagnose["code"]>;
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3,
+}
+
+interface discharge {
+  date: string;
+  criteria: string;
+}
+
+interface sickLeave {
+  startDate: string;
+  endDate: string;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge?: discharge;
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface OccupationalHealthCareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: sickLeave;
+}
+
+export type Entry = HospitalEntry | OccupationalHealthCareEntry | HealthCheckEntry;
 
 export interface Patient {
   id: string;
@@ -17,6 +66,7 @@ export interface Patient {
   gender: Gender;
   ssn?: string;
   dateOfBirth?: string;
+  entries: Entry[];
 }
 
 export type PatientFormValues = Omit<Patient, "id" | "entries">;
